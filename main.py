ce13dcd9feb42032fce25414f3a554e76e6e4a2c
Marvin <44047272+marvinxu99@users.noreply.github.com>
2020-01-05 19:50:15 -0800

    added main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+#
+# Facebook Field Guide to Machine Learning
+# https://research.fb.com/blog/2018/05/the-facebook-field-guide-to-machine-learning-video-series/
+# https://www.anaconda.com/
+#
+# 1 - import the data
+# 2 - Clean the data
+# 3 - Split data. Training Set/Test set
+# 4 - Create a Model
+# 5 - Check the output
+
+
+# numpy - list, array,
+# pandas - tabular data, e.g., reading .csv files
+# scikit-learn - classification, regression, clustering
+# matplotlib - visualize the data
+# jupyter notebooks  - installed from Anaconda
+
+# Kaggle - datasets, competitions
+# kaggle.com
+
+# find out the file path:
+# import os
+# notebook_path = os.path.abspath('soccer.ipynb')
+# print(notebook_path)
+
+
+# Convert the string 2.90K to 2900 or 5.2M to 5200000 in pandas dataframe
+# https://stackoverflow.com/questions/39684548/convert-the-string-2-90k-to-2900-or-5-2m-to-5200000-in-pandas-dataframe
+
+
+# Bokeh
+# https://docs.bokeh.org/en/latest/docs/user_guide/quickstart.html
+#  
